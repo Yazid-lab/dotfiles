ac7ffdec78b602d57e85e757d1dd266a2135f5e6
Yazid <yazidbougrine99@gmail.com>
2022-10-08 20:39:20 +0100

    changed colors to catppuccin

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,30 +1,65 @@
-require("tokyonight").setup({
-	-- your configuration comes here
-	-- or leave it empty to use the default settings
-	style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-	transparent = true, -- Enable this to disable setting the background color
-	terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-	styles = {
-		-- Style to be applied to different syntax groups
-		-- Value is any valid attr-list value for `:help nvim_set_hl`
-		comments = { italic = true },
-		keywords = { italic = true },
-		functions = {},
-		variables = {},
-		-- Background styles. Can be "dark", "transparent" or "normal"
-		sidebars = "transparent", -- style for sidebars, see below
-		floats = "dark", -- style for floating windows
+-- require("tokyonight").setup({
+-- 	-- your configuration comes here
+-- 	-- or leave it empty to use the default settings
+-- 	style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+-- 	transparent = true, -- Enable this to disable setting the background color
+-- 	terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+-- 	styles = {
+-- 		-- Style to be applied to different syntax groups
+-- 		-- Value is any valid attr-list value for `:help nvim_set_hl`
+-- 		comments = { italic = true },
+-- 		keywords = { italic = true },
+-- 		functions = {},
+-- 		variables = {},
+-- 		-- Background styles. Can be "dark", "transparent" or "normal"
+-- 		sidebars = "transparent", -- style for sidebars, see below
+-- 		floats = "dark", -- style for floating windows
+-- 	},
+-- 	sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+-- 	day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+-- 	hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+-- 	dim_inactive = true, -- dims inactive windows
+-- 	lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+--
+-- 	--- You can override specific color groups to use other groups or a hex color
+-- 	--- function will be called with a ColorScheme table
+-- })
+-- vim.cmd("color tokyonight")
+require("catppuccin").setup({
+	compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
+	transparent_background = true,
+	term_colors = true,
+	dim_inactive = {
+		enabled = false,
+		shade = "dark",
+		percentage = 0.15,
 	},
-	sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-	day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-	hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-	dim_inactive = true, -- dims inactive windows
-	lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
-
-	--- You can override specific color groups to use other groups or a hex color
-	--- function will be called with a ColorScheme table
+	styles = {
+		comments = { "italic" },
+		conditionals = { "italic" },
+		loops = {},
+		functions = {},
+		keywords = {},
+		strings = {},
+		variables = {},
+		numbers = {},
+		booleans = {},
+		properties = {},
+		types = {},
+		operators = {},
+	},
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		nvimtree = true,
+		telescope = true,
+		treesitter = true,
+		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+	},
+	color_overrides = {},
+	custom_highlights = {},
 })
-vim.cmd("color tokyonight")
+vim.cmd([[colorscheme catppuccin]])
 local hl = function(thing, opts)
 	vim.api.nvim_set_hl(0, thing, opts)
 end
--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -1,68 +1,71 @@
 return require("packer").startup(function()
-  use("wbthomason/packer.nvim") --speed up loading
-  use("lewis6991/impatient.nvim") -- speed up startup
-  use("nathom/filetype.nvim")
-  --colors
-  --tokiooooooooooooooo
-  use("folke/tokyonight.nvim")
-  use("rebelot/kanagawa.nvim")
-  use("mortepau/codicons.nvim") -- dev icons
-  use({
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  })
-  use("nvim-treesitter/nvim-treesitter-context") --shows the function or class we're inside
-  use("windwp/nvim-ts-autotag") -- hopefully makes autotag work
-  use({
-    "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup({})
-    end,
-  }) --this will pair parentheses and shit
-  --telescope
-  use({
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.0",
-    requires = { { "nvim-lua/plenary.nvim" } },
-  })
-  use({ "nvim-telescope/telescope-file-browser.nvim" })
-  --lualine
-  use({
-    "nvim-lualine/lualine.nvim",
-    requires = { "kyazdani42/nvim-web-devicons", opt = true },
-  })
-  --devicons
-  use({ "kyazdani42/nvim-web-devicons" })
-  --lsp
-  use("nvim-lua/plenary.nvim") -- some module that does shit
-  use("hrsh7th/nvim-cmp") -- Autocompletion plugin
-  use("hrsh7th/cmp-buffer")
-  use("hrsh7th/cmp-path")
-  use("hrsh7th/cmp-nvim-lua")
-  use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
-  use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
-  use("onsails/lspkind.nvim") --add vscode like pictograms and also list the source of the snippet
-  use("L3MON4D3/LuaSnip") -- Snippets plugin
-  use("tpope/vim-fugitive") -- git commands inside nvim
-  use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
-  use("tpope/vim-rhubarb") --Fugitive-companion to interact with github
-  use("numToStr/Comment.nvim") -- plugin that managers commenting code Ex: "gc" to comment visual regions/lines
-  use({ "williamboman/mason.nvim" }) -- manage external editor tooling such as LSP servers, DAP servers, linters, and formatters through a single interface.
-  use("williamboman/mason-lspconfig.nvim")
-  use("neovim/nvim-lspconfig")
-  use("rafamadriz/friendly-snippets") --snippet collection
-  use("jose-elias-alvarez/null-ls.nvim")
-  -- Lua
-  use({
-    "folke/todo-comments.nvim",
-    requires = "nvim-lua/plenary.nvim",
-    config = function()
-      require("todo-comments").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      })
-    end,
-  })
-  use("tpope/vim-sleuth") -- Detect tabstop and shiftwidth automatically
+	use("wbthomason/packer.nvim") --speed up loading
+	use("lewis6991/impatient.nvim") -- speed up startup
+	use("nathom/filetype.nvim")
+	--colors
+	--tokiooooooooooooooo
+	use("folke/tokyonight.nvim")
+	use({ "catppuccin/nvim", as = "catppuccin" })
+	use("rebelot/kanagawa.nvim")
+	use("mortepau/codicons.nvim") -- dev icons
+	use("lukas-reineke/indent-blankline.nvim")
+
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	use("nvim-treesitter/nvim-treesitter-context") --shows the function or class we're inside
+	use("windwp/nvim-ts-autotag") -- hopefully makes autotag work
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	}) --this will pair parentheses and shit
+	--telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({ "nvim-telescope/telescope-file-browser.nvim" })
+	--lualine
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+	--devicons
+	use({ "kyazdani42/nvim-web-devicons" })
+	--lsp
+	use("nvim-lua/plenary.nvim") -- some module that does shit
+	use("hrsh7th/nvim-cmp") -- Autocompletion plugin
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-nvim-lua")
+	use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
+	use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
+	use("onsails/lspkind.nvim") --add vscode like pictograms and also list the source of the snippet
+	use("L3MON4D3/LuaSnip") -- Snippets plugin
+	use("tpope/vim-fugitive") -- git commands inside nvim
+	use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
+	use("tpope/vim-rhubarb") --Fugitive-companion to interact with github
+	use("numToStr/Comment.nvim") -- plugin that managers commenting code Ex: "gc" to comment visual regions/lines
+	use({ "williamboman/mason.nvim" }) -- manage external editor tooling such as LSP servers, DAP servers, linters, and formatters through a single interface.
+	use("williamboman/mason-lspconfig.nvim")
+	use("neovim/nvim-lspconfig")
+	use("rafamadriz/friendly-snippets") --snippet collection
+	use("jose-elias-alvarez/null-ls.nvim")
+	-- Lua
+	use({
+		"folke/todo-comments.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("todo-comments").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
+	use("tpope/vim-sleuth") -- Detect tabstop and shiftwidth automatically
 end)
