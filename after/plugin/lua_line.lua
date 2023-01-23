42478381f792d74f432f83daa7a87e9defd95633
Yazid <yazidbougrine99@gmail.com>
2023-01-23 14:37:00 +0100

    added new sections

--- after/plugin/lua_line.lua
+++ after/plugin/lua_line.lua
@@ -1,7 +1,15 @@
+local function fg(name)
+	return function()
+		---@type {foreground?:number}?
+		local hl = vim.api.nvim_get_hl_by_name(name, true)
+		return hl and hl.foreground and { fg = string.format("#%06x", hl.foreground) }
+	end
+end
+
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = "catppuccin",
+		theme = "auto",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
@@ -19,23 +27,56 @@ require("lualine").setup({
 	},
 	sections = {
 		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { {
-			"filename",
-			file_status = true,
-			path = 2,
-		} },
-		lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_y = { "progress" },
-		lualine_z = { "location" },
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
+		lualine_b = { "branch" },
+		lualine_c = {
+			{
+				"diagnostics",
+				symbols = {
+					error = " ",
+					warn = " ",
+					info = " ",
+					hint = " ",
+				},
+			},
+			{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+			{ "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+			-- stylua: ignore
+			{
+				function() return require("nvim-navic").get_location() end,
+				cond = function() return package.loaded["nvim-navic"] and require("nvim-navic").is_available() end,
+			},
+		},
+		lualine_x = {
+			-- stylua: ignore
+			{
+				function() return require("noice").api.status.command.get() end,
+				cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+				color = fg("Statement")
+			},
+			-- stylua: ignore
+			{
+				function() return require("noice").api.status.mode.get() end,
+				cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+				color = fg("Constant"),
+			},
+			{
+				"diff",
+				symbols = {
+					added = " ",
+					modified = " ",
+					removed = " ",
+				},
+			},
+		},
+		lualine_y = {
+			{ "progress", separator = "", padding = { left = 1, right = 0 } },
+			{ "location", padding = { left = 0, right = 1 } },
+		},
+		lualine_z = {
+			function()
+				return " " .. os.date("%R")
+			end,
+		},
 	},
 	tabline = {},
 	winbar = {},
